de004d5e6a1ef3eaafba6084b84c87c0d68eb402
unsteadyflow <du021000@gmail.com>
2015-11-29 20:39:09 +0900

    액티브 어드민 로그인 프로세스 설정 변경

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  devise_for :users, ActiveAdmin::Devise.config
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   get 'stores/:id' => 'stores#show'
