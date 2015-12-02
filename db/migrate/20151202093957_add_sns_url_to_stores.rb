1cc8b2d3de425456094b3ef334ca81b6a4a7f5a7
unsteadyflow <du021000@gmail.com>
2015-12-02 19:01:06 +0900

    페이스북, 트위터 컬럼 제거

--- db/migrate/20151202093957_add_sns_url_to_stores.rb
+++ db/migrate/20151202093957_add_sns_url_to_stores.rb
@@ -1,7 +1,5 @@
 class AddSnsUrlToStores < ActiveRecord::Migration
   def change
-    add_column :stores, :twitter_id, :string
-    add_column :stores, :facebook_id, :string
     add_column :stores, :background_image, :string
     add_column :stores, :cover_image, :string
     add_column :stores, :url_name, :string
