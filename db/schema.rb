413f919802c27a7013685d2a4cea0034abe5f1a9
unsteadyflow <du021000@gmail.com>
2015-12-05 00:14:24 +0900

    컨텍트 마이그레이션 파일과 모델 파일 작성하고 컨텍트 테이블 만들기

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203123832) do
+ActiveRecord::Schema.define(version: 20151204150935) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20151203123832) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "email"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string   "name"
